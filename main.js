d681b732b13910d7c74b550b83b22f42e80eff62
chayakon11643 <chayakon.prathumchai@gmail.com>
2024-05-16 11:08:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+//let names = ['Sofia', 'David', 'Juan'];
+//console.log(names);
+
+
+//names.push("Sara","Augustin");
+//console.log(names);
+
+
+//names.shit  ();
+//console.log(names);
+
+
+//names.splice(1,0, "Renata");
+//console.log(names);
+
+
+//names.push("Elena");
+//console.log(names);
+
+const bankPeople = [" Sofia" , "David" , "Juan"]; //สร้างอาเรย์ bankPeople ที่มีสมาชิก "Sofia", "David", และ "Juan". แสดงค่าในอาเรย์ bankPeople
+console.log(bankPeople);
+
+bankPeople.push("Sara"); //เพิ่มค่า "Sara" และ "Augstin" ไปยังท้ายอาเรย์:
+bankPeople.push("Augstin");
+console.log(bankPeople);
+
+bankPeople.shift(); //ลบค่าตัวแรก ("Sofia") ของอาเรย์ bankPeople. แสดงค่าในอาเรย์ bankPeople
+console.log(bankPeople);
+
+
+bankPeople.splice(1,0, "Ranata"); //แทรก "Ranata" ที่ตำแหน่งที่ 1 ของอาเรย์ bankPeople.
+console.log(bankPeople);
+
+bankPeople.push("Elena"); //เพิ่ม "Elena" ไปยังท้ายอาเรย์ bankPeople.
+console.log(bankPeople);
+
+
+
+
+
+
+
+
